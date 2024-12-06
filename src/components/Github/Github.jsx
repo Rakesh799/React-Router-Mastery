7d6c6a8561f71e9827712ce3753078fa51ae4540
Rakesh799 <rakeshchoudhury074@gmail.com>
2024-12-06 12:09:04 +0530

    minor update

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -17,7 +17,7 @@ const Github = () => {
     return (
         <div className='w-3/4 mx-auto my-4 h-[63vh] '>
             <img className='w-80 h-80 mx-auto rounded-xl' src={data.avatar_url} alt="Git Profile-Picture" />
-            <h1 className='md:w-80 w-full mx-auto my-2 rounded-xl font-bold text-center p-2 bg-purple-300'>Github Followers: {data.followers} </h1>
+            <h1 className='md:w-80 w-full mx-auto my-2 rounded-xl font-bold text-center p-2 bg-purple-300'>Github Repos: {data.public_repos} </h1>
         </div>
     )
 }
